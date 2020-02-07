02148b42b016ff0ddc31ad3b5c851032fb3ae87e
rchaser53 <tayoshizawa29@gmail.com>
2020-02-07 11:54:16 +0900

    improve css

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,12 @@ class ChildComponent extends React.PureComponent {
       clickEvent, emitMessage, label
     } = this.props;
     console.log(emitMessage);
-    return <div onClick={clickEvent}>{label}</div>
+    return <div
+      className={css.child}
+      onClick={clickEvent}>
+        {label}
+    </div>
   }
-}
-
-const wrapper = {
-  display: "flex",
 }
 
 // 一度しか関数は生成されないがpropsやstateが使えない
@@ -28,37 +28,32 @@ function HomePage() {
     console.log("clicked componentB!")
   }, []);
 
-  return <div>
-    <div className={css.example}>
-      <ChildComponent
-        clickEvent={
-          // インラインハンドラ
-          // 毎回関数が生成されるため再レンダリングが発生する
-          () => {
-            console.log("clicked componentA!")
+  return <div className={css.outline}>
+      <div className={css.parentArea}>
+        <button onClick={() => {
+          setCount(count + 1)
+        }}>increment</button>
+        parent component count: {count}
+      </div>
+      <div className={css.childrenArea}>
+        <ChildComponent
+          clickEvent={
+            // インラインハンドラ
+            // 毎回関数が生成されるため再レンダリングが発生する
+            () => {
+              console.log("clicked componentA!")
+            }
           }
-        }
-        emitMessage={"componentA rendered"}
-        label={"componentA"}
-      />
-      <ChildComponent
-        clickEvent={callback}
-        emitMessage={"componentB rendered"}
-        label={"componentB"}
-      />
-    </div>
-
-      <button onClick={() => {
-        setCount(count + 1)
-      }}>increment</button>
-      {count}
+          emitMessage={"componentA rendered"}
+          label={"child componentA"}
+        />
+        <ChildComponent
+          clickEvent={callback}
+          emitMessage={"componentB rendered"}
+          label={"child componentB"}
+        />
+      </div>
   </div>
 }
 
 export default HomePage
-
-
-// function areEqual(prevProps, nextProps) {
-//   return false;
-// }
-// const MemoComponent = React.memo(ComponentA, areEqual);
